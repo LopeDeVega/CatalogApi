f331acfc0a187232c8d0954b551c146c1bec0650
LopeDeVega <sentinel_red@live.com>
2023-03-21 16:47:56 +0000

    Before Dependency Injection

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using Catalog.Entities;
+
+namespace Catalog.Controllers;
+
+
+// ControllerBase turn the class into a contraller-class (inheriting all the necessary tools)
+[ApiController]
+[Route("item")]
+public class ItemsController : ControllerBase
+{
+    private readonly InMemItiemsRepository repository;
+
+    //Contructor
+    public ItemsController()
+    {
+        repository = new InMemItiemsRepository();
+    }
+
+    //Http verb // atribute
+    [HttpGet]
+    // Get a list of items stored stored the InMeItiemsRepository
+    public IEnumerable<Item> GetItems()
+    {
+        var item = repository.GetItems();
+        return item;
+    }
+
+    //ActonResult allow to return more than one thing
+    [HttpGet("{id}")]
+    public ActionResult<Item> GetItem(Guid id)
+    {
+        var item = repository.GetItem(id);
+
+        //Return a status code when the item is not found
+        if(item == null)
+        {
+            return NotFound();
+        }
+
+        return item;
+    }
+
+
+
+}
---
+++ Repositories/InMemItiemsRepository.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories;
+
+
+public class InMemItiemsRepository
+{
+    //List of Item
+    List<Item> items = new List<Item>()
+    {
+        new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreateDate = DateTimeOffset.UtcNow },
+        new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreateDate = DateTimeOffset.UtcNow },
+        new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreateDate = DateTimeOffset.UtcNow }
+    };
+
+    // Get method to retrive an items
+    public IEnumerable<Item> GetItems()
+    {
+        return items;
+    }
+
+    public Item GetItem(Guid id)
+    {
+        if (string.IsNullOrEmpty(items.ToString()))
+        {
+            Console.WriteLine("The Item is null");
+        }
+
+        return items.Where(item => item.Id == id).SingleOrDefault();
+    }
+
+}
